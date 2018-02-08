f713229dcf2c174a6fea3ffd593da24340a05caa
Will Allan <will.allan@bamtechmedia.com>
2018-02-07 17:35:38 -0800

    Stuff

--- src/5/js/AudioVisualization.js
+++ src/5/js/AudioVisualization.js
@@ -41,6 +41,54 @@ template.innerHTML = `
   <span></span>
 `;
 
+const events = [
+  "abort",
+  "canplay",
+  "canplaythrough",
+  "durationchange",
+  "emptied",
+  "ended",
+  "error",
+  "loadeddata",
+  "loadedmetadata",
+  "loadstart",
+  "pause",
+  "play",
+  "playing",
+  "progress",
+  "ratechange",
+  "seeked",
+  "seeking",
+  "stalled",
+  "suspend",
+  "timeupdate",
+  "volumechange",
+  "waiting"
+];
+
+const properties = [
+  { name: "autoplay", readonly: false, attribute: true },
+  { name: "controls", readonly: false, attribute: true },
+  { name: "currentTime", readonly: false, attribute: false },
+  { name: "defaultMuted", readonly: false, attribute: false },
+  { name: "duration", readonly: true, attribute: false },
+  { name: "ended", readonly: true, attribute: false },
+  { name: "error", readonly: true, attribute: false },
+  { name: "loop", readonly: false, attribute: true },
+  { name: "muted", readonly: false, attribute: true },
+  { name: "paused", readonly: true, attribute: false },
+  { name: "preload", readonly: false, attribute: true },
+  { name: "seeking", readonly: true, attribute: false },
+  { name: "src", readonly: false, attribute: true },
+  { name: "volume", readonly: false, attribute: false }
+];
+
+const methods = [
+  "load",
+  "pause",
+  "play"
+]
+
 class AudioVisualization extends HTMLElement {
   constructor() {
     super();
@@ -51,8 +99,8 @@ class AudioVisualization extends HTMLElement {
       const shadowNode = template.content.cloneNode(true);
 
       // Initialize audio
-      const audioElement = shadowNode.querySelector("audio");
       const audioContext = new AudioContext();
+      const audioElement = this.audioElement = shadowNode.querySelector("audio");
       const audioSourceNode = audioContext.createMediaElementSource(audioElement);
       const audioGainNode = audioContext.createGain();
       const audioAnalyserNode = audioContext.createAnalyser();
@@ -108,6 +156,53 @@ class AudioVisualization extends HTMLElement {
   paint() {
     console.log(`${this.id || "(unknown)"}.paint`);
   }
+
+  // fake(x, y, z) {
+  //   console.log(`${this.id || "(unknown)"}.fake`, { x, y, z });
+  //   return true;
+  // }
 }
+
+// for (const method of methods) {
+//   AudioVisualization.prototype[method] = function (...args) {
+//     console.log(`${this.id || "(unknown)"}.${method}`, args);
+//     return this.audioElement[method](...args);
+//   };
+// }
+
+// Object.keys(descriptors4).forEach((key) => {
+//   Object.defineProperty(AudioVisualization.prototype, key, descriptors4[key]);
+// });
+
+// AudioVisualization.HAVE_NOTHING = HTMLMediaElement.HAVE_NOTHING;
+// AudioVisualization.HAVE_METADATA = HTMLMediaElement.HAVE_METADATA;
+// AudioVisualization.HAVE_CURRENT_DATA = HTMLMediaElement.HAVE_CURRENT_DATA;
+// AudioVisualization.HAVE_FUTURE_DATA = HTMLMediaElement.HAVE_FUTURE_DATA;
+// AudioVisualization.HAVE_ENOUGH_DATA = HTMLMediaElement.HAVE_ENOUGH_DATA;
+
+// AudioVisualization.NETWORK_EMPTY = HTMLMediaElement.NETWORK_EMPTY;
+// AudioVisualization.NETWORK_IDLE = HTMLMediaElement.NETWORK_IDLE;
+// AudioVisualization.NETWORK_LOADING = HTMLMediaElement.NETWORK_LOADING;
+// AudioVisualization.NETWORK_NO_SOURCE = HTMLMediaElement.NETWORK_NO_SOURCE;
+
+function getDescriptors(o) {
+  return Object.getOwnPropertyNames(o).reduce((obj, name) => {
+    var descriptor = Object.getOwnPropertyDescriptor(o, name);
+    if (descriptor.enumerable) {
+      return Object.assign(obj, { [name]: descriptor });
+    } else {
+      return obj;
+    }
+  }, {});
+}
+
+document.createElement("audio").NETWORK_EMPTY
+
+console.log(getDescriptors(HTMLAudioElement));
+console.log(getDescriptors(HTMLAudioElement.prototype));
+console.log(getDescriptors(HTMLMediaElement));
+console.log(getDescriptors(HTMLMediaElement.prototype));
+console.log(getDescriptors(AudioVisualization));
+console.log(getDescriptors(AudioVisualization.prototype));
 
 export default AudioVisualization;

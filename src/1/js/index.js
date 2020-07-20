f370b119c3990156f8635a09af393f98a7e02d38
Will Allan <will.allan@disneystreaming.com>
2020-07-19 23:28:00 -0700

    Fix URLs

--- src/1/js/index.js
+++ src/1/js/index.js
@@ -6,7 +6,7 @@
 */
 
 var media = [
-      "/audio/new_year_dubstep_minimix.ogg"
+      "/content/audio/new_year_dubstep_minimix.ogg"
    ],
     fftSize = 1024,
     // [32, 64, 128, 256, 512, 1024, 2048]

967afce058be2bcb2c7533be0067c60af191fe3c
Will Allan <billyzkid@yahoo.com>
2020-07-22 07:29:35 -0700

    Updates

--- src/8/js/index.js
+++ src/8/js/index.js
@@ -1,11 +1,3 @@
 import AudioVisualization from "./AudioVisualization.js";
-import Visualization from "./Visualization.js";
 
-// Define the custom <audio-visualization /> element
 customElements.define("audio-visualization", AudioVisualization);
-
-// Assign an AudioContext to each element
-// document.querySelectorAll("audio-visualization").forEach((element) => {
-//     element.visualization = new Visualization();
-//     element.visualization.load("/content/audio/new_year_dubstep_minimix.ogg");
-// });
